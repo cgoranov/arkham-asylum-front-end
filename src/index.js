958a0871c8b254fbd4452f7aa7add44f82aae6cf
Che <cgoranov@gmail.com>
2022-04-30 13:45:01 -0500

    added .js files for remaining componenets to start organizing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/store'
+import store from './redux/Store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store} >
