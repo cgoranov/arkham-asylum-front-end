8e86b84662e1dac8fb0e182e9c34954b2d109eb4
Che <cgoranov@gmail.com>
2022-04-30 11:47:30 -0500

    export store and import store in index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider>
-      <App />
+      <App store={store} />
     </Provider>
   </React.StrictMode>
 );
