8e82f646a32f345699a4a2735c23c398fb62a7eb
Che <cgoranov@gmail.com>
2022-04-30 15:31:10 -0500

    build out setCharacters and fetch characters actions creators

---
+++ src/actions/actions.js
@@ -0,0 +1,12 @@
+import { SET_CHARACTERS } from "./actionTypes"
+
+const setCharacters = (characters) => ({type: SET_CHARACTERS, payload: characters})
+
+export const fetchCharacters = () => {
+    return dispatch => (
+        fetch("http://localhost:3000/characters")
+        .then(r=> r.json())
+        .then(characters => dispatch(setCharacters(characters)))
+        .catch(err => console.error(err))
+    )
+} 
