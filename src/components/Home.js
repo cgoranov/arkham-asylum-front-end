8010b91f5239c2518fc2dd6614fea89cd8eccb00
Che <cgoranov@gmail.com>
2022-04-30 14:39:19 -0500

    wrote home page welcome message

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,13 @@
 
 function Home(){
     return (
-        "Home Page"
+        <div>
+            <h1>Welcome to Arkham Aslyum!</h1>
+            <p className="homeintrodcution">The Elizabeth Arkham Asylum for the Criminally Insane, commonly referred to as Arkham Asylum, houses the most dangerouse of Batman 
+            villains. Enter at your own risk. This is only for the most die-hard Batman fans. If you are ready, proceed to the instructions section and we will guide you on the
+            upcoming experience.  
+             </p>
+        </div>
     )
 }
 
