9835f9607925784fff5b1ed7ec3829bd3239e709
Che <cgoranov@gmail.com>
2022-04-30 19:54:48 -0500

    added Batman icon and formatting CSS for all icons as well as text blocks for each villain

--- src/components/characters/Batman.js
+++ src/components/characters/Batman.js
@@ -1,7 +1,9 @@
 function Batman(props){
+
+    
+
     return (
         <div className="Batman" key={props.batman.id}>
-            <p> "The Batman" </p>
         </div>
     )
 
--- src/components/characters/Villains.js
+++ src/components/characters/Villains.js
@@ -2,7 +2,7 @@
 function Villains(props) {
     return (
         <div className={props.villain.name}>
-            <div className="villain-text_block">
+            <div className="villain-text-block">
                 <p>{props.villain.name}</p>
                 <p>{props.villain.fun_fact}</p>
             </div>
