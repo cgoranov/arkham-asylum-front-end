e4a946c95b6cdf619c1b867b633803593d26e77d
Che <cgoranov@gmail.com>
2022-05-01 11:20:34 -0500

    handleSubmit and handleChange initial buildouts

--- src/components/mostWanted/MostWantedForm.js
+++ src/components/mostWanted/MostWantedForm.js
@@ -5,9 +5,13 @@ function MostWantedForm() {
     const [name, setName] = useState("")
     const [funFact, setFunFact] = useState("")
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    } 
+
     return (
-        <form>
-            <input type="text" placeholder="name" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="name" onChange={handleNameChange} />
             <input type="text" placeholder="fun_fact" />
             <input type="submit" />
         </form>
