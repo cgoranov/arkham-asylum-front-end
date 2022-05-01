e1cd7c2616175eaf07f1fb7e01506dadafaa5055
Che <cgoranov@gmail.com>
2022-04-30 23:24:08 -0500

    end of night save point

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -4,7 +4,7 @@ function Instructions(){
             <h1>Instructions</h1>
             <h2>You brave soul. I see no fear in your eyes. You are a true Dark Knight fan. Your next steps are below: </h2>
             <ol type="1">
-                <li>Navigate to Arkham Asylum where your avatar can interact with the different villains that are currently
+                <li>Navigate to Arkham Asylum where you can interact with the different villains that are currently
                  captured and learn more about these famous rogues by clicking on their icons. </li>
                  <li>However, you will notice, as a true fan, there are many missing criminals. Navigate to "Most Wanted" where you can add a Villain that you see is missing
                  and fun fact about them. After all, Jim Gordan and the GCPD need your knowledge to know what threats still lurk out there.</li>
