9058839f4b50d226e0a4ef087436c5a47323efce
Che <cgoranov@gmail.com>
2022-04-30 20:39:15 -0500

    finalize handleClick event handler for Arkham map

--- src/components/characters/Villains.js
+++ src/components/characters/Villains.js
@@ -1,16 +1,19 @@
 
 function Villains(props) {
 
-    const handleClick= (e) => {
-        e.target.innerHTML = 
-        `<div className="villain-text-block">
-                <p>${props.villain.name}</p>
-                <p>${props.villain.fun_fact}</p>
-        </div>`
+    const handleClick = (e) => {
+        console.log("event triggered!")
+        const element = e.target.children[0]
+        element.style.visibility='visible'
+        
     }
 
     return (
         <div className={props.villain.name} onClick={handleClick}>
+            <div className="villain-text-block" >
+                <p>{props.villain.name}</p>
+                <p>{props.villain.fun_fact}</p>
+            </div>
         </div>
     )
 }
