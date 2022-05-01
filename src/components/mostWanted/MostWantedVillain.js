dd0d747d7a8e68dcb31681cc7155cb4881356770
Che <cgoranov@gmail.com>
2022-05-01 14:21:25 -0500

    successful implementation of Wanted Vilalins and styling

--- src/components/mostWanted/MostWantedVillain.js
+++ src/components/mostWanted/MostWantedVillain.js
@@ -1,5 +1,12 @@
 function MostWantedVillain(props) {
 
+    return (
+        <div className="MostWanted">
+            <p>{props.villain.name}</p>
+            <p>{props.villain.fun_fact}</p>
+
+        </div>
+    )
  
 }
 
