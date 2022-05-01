a6fdd78e58672b2b122f1646e885b397047d196c
Che <cgoranov@gmail.com>
2022-04-30 19:46:28 -0500

    div element with class text-block for each Villain component

--- src/components/characters/Villains.js
+++ src/components/characters/Villains.js
@@ -2,8 +2,10 @@
 function Villains(props) {
     return (
         <div className={props.villain.name}>
-            <p>{props.villain.name}</p>
-            <p>{props.villain.fun_fact}</p>
+            <div className="villain-text_block">
+                <p>{props.villain.name}</p>
+                <p>{props.villain.fun_fact}</p>
+            </div>
         </div>
     )
 }
