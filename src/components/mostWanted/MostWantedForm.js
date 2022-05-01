686c88f59792c7eb1f8339cf06edeaa614d469c8
Che <cgoranov@gmail.com>
2022-05-01 11:12:30 -0500

    Most Wanted form build-out

---
+++ src/components/mostWanted/MostWantedForm.js
@@ -0,0 +1,11 @@
+function MostWantedForm() {
+
+    return (
+        <form>
+            <input type="text" placeholder="name" />
+            <input type="text" placeholder="fun_fact" />
+            <input type="submit" />
+        </form>
+    )
+
+}
